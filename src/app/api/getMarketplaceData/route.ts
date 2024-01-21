8fbfadc30a9ba1c7984c24ee244660297d7cacd0
alvolex <alexandervolkers@gmail.com>
2024-01-21 23:36:24 +0100

    ads endpoint to fetch all bundles from epic, removes hardcoded bearer token

---
+++ src/app/api/getMarketplaceData/route.ts
@@ -0,0 +1,38 @@
+export async function GET(request: Request) {
+  var myHeaders = new Headers({
+    Cookie: "EPIC_BEARER_TOKEN=XXXXXXXXXXXXXXXXXXXXXXXXX",
+  });
+
+  var requestOptions: RequestInit = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  async function fetchData(start = 0) {
+    try {
+      const response = await fetch(
+        `https://www.unrealengine.com/marketplace/api/assets/vault?lang=en-US&start=${start}&count=100`,
+        requestOptions
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      let data = await response.json();
+      return data.data;
+    } catch (error) {
+      console.log("error", error);
+      return new Response("Error occurred", { status: 500 });
+    }
+  }
+
+  let data = await fetchData();
+  while(data.paging.start < data.paging.total) {
+    let newData = await fetchData(data.paging.start + 100);
+    data.elements = data.elements.concat(newData);
+    data.paging.start = newData.paging.start;
+  }
+
+  return new Response(JSON.stringify(data), { status: 200 });
+}
